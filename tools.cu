daf1055d91dda9952d1eaae7800e678efd89b2d5
jozech <jose.chavez.alvarez@ucsp.edu.pe>
2018-11-16 13:39:31 -0500

    tools: code one loop per sent

--- tools.cu
+++ tools.cu
@@ -44,20 +44,23 @@ unsigned char* readBMPFile( char const*  filename,
     int sizeIH = *(int *)&infoHeaderSize[0];
     //int sizeIH = infoHeaderSize[3] << 24 | infoHeaderSize[2] << 16 | infoHeaderSize[1] << 8 | infoHeaderSize[0];
     
-    printf("Size of InfoHeader: %d\n", sizeIH);
+    printf("\nSize of InfoHeader: %d\n", sizeIH);
 
     unsigned char infoHeader[sizeIH - 4];
 
     fread(infoHeader, sizeof(unsigned char), sizeIH - 4 , f);
+    
+    width           = *(int *)&infoHeader[0];
+    height          = *(int *)&infoHeader[4];
+    depth           = *(int *)&infoHeader[10]; 
+    
+    int imageSize   = *(int *)&infoHeader[16];
 
-    width   = *(int *)&infoHeader[0];
-    height  = *(int *)&infoHeader[4];
-    depth   = *(int *)&infoHeader[10]; 
-
-    printf("width = %d, height = %d, depth = %d\n",
+    printf("\nwidth = %d, height = %d, depth = %d, imageSize = %d\n",
                                                 width, 
                                                 height,
-                                                depth);  
+                                                depth,
+                                                imageSize);  
     
     int sizeInput;
     int sizeOutput = width * height * 3;
@@ -69,16 +72,19 @@ unsigned char* readBMPFile( char const*  filename,
         int sizeCT = offset - 14 - sizeIH;
         unsigned char ColorTable[sizeCT];
         fread(ColorTable, sizeof(unsigned char), sizeCT, f);
-        printf("Size of ColorTable : %d\n", sizeCT);
+        printf("\nSize of ColorTable : %d\n", sizeCT);
 
-        float pad  = (float)depth / 8;
-        sizeInput  = (int)(width * height * pad);
+        //float pad  = (float)depth / 8;
+        //sizeInput  = (int)(width * height * pad);
         
-        unsigned char* dataEnc = new unsigned char[sizeInput];       
+        unsigned char* dataEnc = new unsigned char[imageSize];       
         
-        fread(dataEnc, sizeof(unsigned char), sizeInput, f);
+        fread(dataEnc, sizeof(unsigned char), imageSize, f);
         fclose(f);    
+        
 
+        unsigned char iClr;
+        /*
         unsigned char indColor0;
         unsigned char indColor1;
         unsigned char indColor2;
@@ -87,10 +93,20 @@ unsigned char* readBMPFile( char const*  filename,
         unsigned char indColor5;
         unsigned char indColor6;
         unsigned char indColor7;
-
-        for (int i = 0; i < sizeInput; i++)
+        */
+        for (int i = 0; i < imageSize; i++)
         {
             if (depth == 1){
+
+                for (int bit = 0; bit < 8; bit++)
+                {
+                    iClr = (dataEnc[i] >> bit) & 1;
+
+                    data[i*24 + 3*(8 - bit - 1)    ]  = ColorTable[iClr*4+1];
+                    data[i*24 + 3*(8 - bit - 1) + 1]  = ColorTable[iClr*4+1];
+                    data[i*24 + 3*(8 - bit - 1) + 2]  = ColorTable[iClr*4+1];
+                }
+                /*
                 unsigned char  X = dataEnc[i];
                 indColor7 = (X >> 0) & 1;
                 indColor6 = (X >> 1) & 1;
@@ -101,7 +117,7 @@ unsigned char* readBMPFile( char const*  filename,
                 indColor1 = (X >> 6) & 1;
                 indColor0 = (X >> 7) & 1;
                 
-                /*
+                
                 printf("ind0 = %2d, ind1 = %2d, ind2 = %2d, ind3 = %2d, ind4 = %2d, ind5 = %2d\n", 
                         indColor0,
                         indColor1,
@@ -109,7 +125,7 @@ unsigned char* readBMPFile( char const*  filename,
                         indColor3,        
                         indColor4, 
                         indColor5);
-                */
+
                 data[i*24    ]  = ColorTable[indColor0*4+1];
                 data[i*24 + 1]  = ColorTable[indColor0*4+1];
                 data[i*24 + 2]  = ColorTable[indColor0*4+1];
@@ -141,8 +157,20 @@ unsigned char* readBMPFile( char const*  filename,
                 data[i*24 + 21] = ColorTable[indColor7*4+1];
                 data[i*24 + 22] = ColorTable[indColor7*4+1];
                 data[i*24 + 23] = ColorTable[indColor7*4+1];
+                */
             }
+
             else if (depth == 4){
+                for (int bit = 0; bit < 8; bit+=4)
+                {
+                    iClr = (dataEnc[i] >> bit) & 15;
+
+                    data[i*6 + 3*(1 - bit/4)    ]  = ColorTable[iClr*4+0];
+                    data[i*6 + 3*(1 - bit/4) + 1]  = ColorTable[iClr*4+1];
+                    data[i*6 + 3*(1 - bit/4) + 2]  = ColorTable[iClr*4+2];
+                }
+
+                /*
                 indColor1 = 15 &  dataEnc[i];
                 indColor0 = 15 & (dataEnc[i] >> 4);
          
@@ -155,14 +183,16 @@ unsigned char* readBMPFile( char const*  filename,
                 data[i*6 + 3] = ColorTable[indColor1*4  ];
                 data[i*6 + 4] = ColorTable[indColor1*4+1];
                 data[i*6 + 5] = ColorTable[indColor1*4+2];
+
+                */
             }
 
             else if (depth == 8){
-                indColor1 = dataEnc[i];
+                iClr = dataEnc[i];
 
-                data[i*3    ] = ColorTable[indColor1*4];
-                data[i*3 + 1] = ColorTable[indColor1*4+1];
-                data[i*3 + 2] = ColorTable[indColor1*4+2];
+                data[i*3    ] = ColorTable[iClr*4];
+                data[i*3 + 1] = ColorTable[iClr*4+1];
+                data[i*3 + 2] = ColorTable[iClr*4+2];
 
             }
 
